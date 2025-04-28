9c38da53c7000864056c057356c84619d5328f2a
Nomad37301 <agungpurnama100@gmail.com>
2025-04-29 07:20:16 +0800

    Revsi Add dummy data pada database

--- src/storage/Database.java
+++ src/storage/Database.java
@@ -5,12 +5,25 @@ import model.Portofolio;
 import model.Saham;
 import model.SuratBerhargaNegara;
 
+import java.time.LocalDate;
 import java.util.*;
 
 public class Database {
     private static final List<Saham> sahamList = new ArrayList<>();
     private static final List<SuratBerhargaNegara> sbnList = new ArrayList<>();
     private static final Map<Customer, Portofolio> portofolioMap = new HashMap<>();
+
+    static {
+        // Dummy Data Saham
+        sahamList.add(new Saham("BBRI", "Bank BRI", 5000));
+        sahamList.add(new Saham("TLKM", "Telkom Indonesia", 4500));
+        sahamList.add(new Saham("ASII", "Astra International", 7000));
+
+        // Dummy Data SBN
+        sbnList.add(new SuratBerhargaNegara("SBR010", 6.0, 24, LocalDate.of(2027, 4, 1), 500_000_000));
+        sbnList.add(new SuratBerhargaNegara("ORI021", 5.5, 36, LocalDate.of(2028, 10, 1), 300_000_000));
+        sbnList.add(new SuratBerhargaNegara("SR017", 5.8, 36, LocalDate.of(2028, 3, 1), 400_000_000));
+    }
 
     public static List<Saham> getSahamList() {
         return sahamList;
