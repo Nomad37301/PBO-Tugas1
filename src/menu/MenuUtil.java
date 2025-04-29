32cb7ddc659878a311063b1a4ae1ea74e7e59740
Nomad37301 <agungpurnama100@gmail.com>
2025-04-29 15:25:09 +0800

    Revisi metode clearscreen

--- src/menu/MenuUtil.java
+++ src/menu/MenuUtil.java
@@ -24,12 +24,8 @@ public class MenuUtil {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
             }
         } catch (Exception ex) {
-            System.out.println("\n\n");
         }
     }
 
